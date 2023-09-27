bbfb39c0f59b2dba3b4fd881c7f397dbbfe85011
Kyle Lastimosa <glitchkyle16@gmail.com>
2023-09-27 17:16:22 -0400

    Update given files

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    print("Starting Pong Server")
+# =================================================================================================
+# Contributing Authors:	    <Anyone who touched the code>
+# Email Addresses:          <Your uky.edu email addresses>
+# Date:                     <The date the file was last edited>
+# Purpose:                  <How this file contributes to the project>
+# Misc:                     <Not Required.  Anything else you might want to include>
+# =================================================================================================
+
+import socket
+import threading
+
+# Use this file to write your server logic
+# You will need to support at least two clients
+# You will need to keep track of where on the screen (x,y coordinates) each paddle is, the score 
+# for each player and where the ball is, and relay that to each client
+# I suggest you use the sync variable in pongClient.py to determine how out of sync your two
+# clients are and take actions to resync the games
