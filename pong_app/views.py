1fea8dc12bf8d46f022fb77081cfcfd75b929de9
Kyle Lastimosa <glitchkyle16@gmail.com>
2023-10-24 20:24:39 -0400

    Complete leaderboard web page

--- pong_app/views.py
+++ pong_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+
+from .models import User
 
 def index(request):
-    return HttpResponse("Hello world!")
+    user_list = User.objects.all()
+    context = {"user_list": user_list}
+    return render(request, "pong_app/index.html", context)
