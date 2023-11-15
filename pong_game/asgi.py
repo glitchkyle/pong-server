203b3ef4c6de92a823e0a6c90fd264cf3d0e0507
Nishan Budhathoki <nishanbudhathoki19@icloud.com>
2023-11-15 14:21:00 -0500

    Adding Comments to pong_game in the server

--- pong_game/asgi.py
+++ pong_game/asgi.py
@@ -2,7 +2,7 @@
 Contributing Authors:	  Nishan Budathoki, James Chen, Kyle Lastimos
 Email Addresses:          nishan.budhathoki@uky.edu, James.Chen@uky.edu, klastimosa001@uky.edu
 Date:                     Nov 11,2023
-Purpose:                  
+Purpose:                  Configures ASGI for the pong_game project to handle asynchronous web requests. 
 """
 """
 ASGI config for project project.
--- pong_game/wsgi.py
+++ pong_game/wsgi.py
@@ -2,7 +2,7 @@
 Contributing Authors:	  Nishan Budathoki, James Chen, Kyle Lastimos
 Email Addresses:          nishan.budhathoki@uky.edu, James.Chen@uky.edu, klastimosa001@uky.edu
 Date:                     Nov 11,2023
-Purpose:                  
+Purpose:                  Configures WSGI for the pong_game project to handle web requests in a production environment. 
 """
 """
 WSGI config for project project.
